88e295fd4ce25b5fec58b97178ceff3b2d230e4b
7827 <84583344+7827@users.noreply.github.com>
2021-06-12 13:19:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,61 @@
+var bullet,wall,thickness;
+var speed,weight;
+
+
+function setup() {
+  createCanvas(1600,400);
+  thickness=random(22,83);
+  bullet= createSprite(400, 200, 100, 20)
+ wall= createSprite(1200,200,thickness,height/2);
+ speed=random(223,321);
+  weight=random(30,52);
+}
+
+function draw() {
+  background("black");
+  bullet.velocityX=speed;
+  bullet.shapeColor="brown"
+
+  if(wall.x-bullet.x<(bullet.width+wall.width)/2){
+    bullet.velocityX=0;
+    var deformation=0.5*weight*speed*speed/22500;
+    if(deformation>180){
+      bullet.shapeColor="yellow"; 
+    }
+    if(deformation<180 && deformation>100){
+      bullet.shapeColor="yellow";
+    }
+    if(deformation<100){
+      bullet.shapeColor="yellow";
+    }
+  }
+  
+  if(hasCollided(bullet,wall))
+  {
+    bullet.velocityX=0;
+    var damage=0.5*weight*speed*speed/(thickness*thickness*thickness);
+  
+  if(damage>10){
+    wall.shapeColor=("red")
+  }
+  if(damage<10){
+    wall.shapeColor=("green")
+  }
+  }
+  
+  
+  
+  
+  drawSprites();
+}
+
+
+function hasCollided(lbullet,lwall)
+{
+  bulletRightEdge=lbullet.x+lbullet.width;
+  wallLeftEdge=lwall.x;
+  if(bulletRightEdge>=wallLeftEdge){
+    return true
+  }
+  return false
+}
